7cc248457ea5b403f073b3054103c7dea46ab114
Benjamin Rivera <benriveracoding@gmail.com>
2021-03-17 17:22:56 -0600

    refactor into venv instead of pipenv

--- mtg-database.py
+++ mtg-database.py
@@ -3,10 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
 def hello_world():
-  return "Hello World"
+    return 'Hello, World!'
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
